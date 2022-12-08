c640aaf5b5679812dcc9f0def43d0852c7147000
KristopherW <kristopherwilmot@gmail.com>
2022-12-08 19:42:14 -0400

    Initial commit of day 6
    updated input.txt

---
+++ 6/part1.py
@@ -0,0 +1,14 @@
+import sys
+
+# Read in the input as a single string
+input = open(sys.argv[1], 'r').read()
+
+# Look at groups of 4 chars, moving one char to the right on each loop
+for i in range(len(input)-3):
+    # Since sets cant contain duplicates, set the string of the four current chars to a set
+    temp_chars = set(input[i:i+4])
+
+    # If the length of the set is 4, we know theres no duplicates.  Return the index of the last char.
+    if len(temp_chars) == 4:
+        print(i+4)
+        break
